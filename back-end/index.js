0a54134cec32a660e47a78b49cf06911a241b0ef
bhavesh <bhaveshvaishnav555@gmail.com>
2024-02-08 23:55:14 +0530

    updated base url

--- back-end/index.js
+++ back-end/index.js
@@ -12,7 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
-app.use(cors())
+app.use(cors(
+  {
+    origin: "https://tinyy-url.netlify.app",
+    credentials: true,
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
+  }
+))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
